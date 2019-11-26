d89b207c79dc9758f6bf9168b86282bf1157e51c
becsyu <33007529+becsyu@users.noreply.github.com>
2019-11-26 03:18:34 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following example utilizes lexicopic searching in R and optimizes lengthy maths functions by caching existing computations and skips the work.
 
-## Write a short comment describing this function
+## First, let's build a function that will return the inverse of any inversible matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) i<<-solve
+	getinverse <- function() i
+	list(set = set, get = get, setinverse = setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Now we write a function that first retrieve the archived cache if the matrix has already been inversed. Otherwise run the solve() function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+        	message ("getting cached data")
+        	return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
